f0008455ad827541e6fdf316fd49b7242956e828
ItsVasconcellos <fernando.vasconcellos@sou.inteli.edu.br>
2023-03-07 14:17:15 -0300

    Alterando lógica da matriz de eisenhower!

--- Matriz de Eisenhower/Node2D.gd
+++ Matriz de Eisenhower/Node2D.gd
@@ -1,25 +1,67 @@
 extends Node2D
 
+#Variáveis de controle de cada campo da matriz
+var impUrg = 0
+var impNurg = 0
+var nImpUrg = 0
+var nImpNurg = 0
 
 func _ready():
-	$HSlider/Label.text = "Tempo: 0h"
-	$HSlider2/Label2.text = "Tempo: 0h"
-	$HSlider3/Label3.text = "Tempo: 0h"
-	$HSlider4/Label4.text = "Tempo: 0h"
-	
 	get_node("HSlider").set_max(24)
 	get_node("HSlider2").set_max(24)
 	get_node("HSlider3").set_max(24)
 	get_node("HSlider4").set_max(24)
 
 func _on_HSlider_value_changed(value):
-	$HSlider/Label.text = "Tempo:" + str($HSlider.value) + "h"
-
+	impUrg= $HSlider.value 
+	if (fazerSomaTotal() <= 24):
+		$HSlider/Label.text = "Tempo:" + str($HSlider.value) + "h"
+	else:
+		$HSlider.value = fazerSomaTotal()-24
+		
 func _on_HSlider2_value_changed(value):
-	$HSlider2/Label2.text = "Tempo:" + str($HSlider2.value) + "h"
+	impNurg= $HSlider2.value 
+	if (fazerSomaTotal() <= 24):
+		$HSlider2/Label2.text = "Tempo:" + str($HSlider2.value) + "h"
+	else:
+		$HSlider2.value = fazerSomaTotal() - 24
 
 func _on_HSlider3_value_changed(value):
-	$HSlider3/Label3.text = "Tempo:" + str($HSlider3.value) + "h"
+	nImpUrg = $HSlider3.value 
+	if (fazerSomaTotal() <= 24):
+		$HSlider3/Label3.text = "Tempo:" + str($HSlider3.value) + "h"
+	else:
+		$HSlider3.value = fazerSomaTotal()-24
 
 func _on_HSlider4_value_changed(value):
-	$HSlider4/Label4.text = "Tempo:" + str($HSlider4.value) + "h"
+	nImpNurg = $HSlider4.value 
+	if (fazerSomaTotal() <= 24):
+		$HSlider4/Label4.text = "Tempo:" + str($HSlider4.value) + "h"
+	else:
+		$HSlider4.value = fazerSomaTotal()-24
+
+#Função de controle para o total de horas não passar de 24hrs
+func fazerSomaTotal():
+	return impUrg + impNurg + nImpNurg + nImpUrg
+	
+
+
+func _on_Button_pressed():
+	$HSlider.hide()
+	$HSlider2.hide()
+	$HSlider3.hide()
+	$HSlider4.hide()
+	
+#	verificaUrgImp()
+#	verificaNurgImp()
+#	verificaUrgNimp()
+#	verificaNurgNimp()
+#
+#func verificaUrgImp():
+#	if 
+#func verificaNurgImp():
+#	pass
+#func verificaUrgNimp():
+#	pass
+#func verificaNurgNimp():
+#	pass
