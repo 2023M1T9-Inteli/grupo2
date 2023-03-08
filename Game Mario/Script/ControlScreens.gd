d219b910fae1e1554d31f1bec0fd2a2b12d79175
Edustn <eduardo.santos@sou.inteli.edu.br>
2023-03-08 10:46:56 -0300

    Mudar esc

--- Game Mario/Script/ControlScreens.gd
+++ Game Mario/Script/ControlScreens.gd
@@ -7,14 +7,19 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.s
+#func _ready():
+#	set_process(true)
 
+func _input(event):
+	if Input.is_action_pressed("ui_cancel"):
+		queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
-
+#	if Input.is_action_pressed("key_exit"):
+#		get_tree().quit()
+		
+			
 #quando botão é pressionado volta para cena anterior
 func _on_returnBtn_pressed():
 	queue_free()
