8045a09af0208a9f16427eb84a5c92523b88b573
LuizaRubim <luiza.rubim@sou.inteli.edu.br>
2023-03-10 00:28:06 -0300

    Adicionando o popUp sobre o inimigo!

--- Game Mario/Cenas/World.gd
+++ Game Mario/Cenas/World.gd
@@ -1,5 +1,12 @@
 extends Node2D
+var video = true
 
 func _ready():
 	ControleMusica.play_musica()
-	$"popUpRelógio".hide()
+	$"popUpRelogio".hide()
+
+func _process(delta):
+	if(video):
+		if($Player.position.x >= 475):
+			$popUpRelogio/Popup.ativaPopUp()
+			video = false
