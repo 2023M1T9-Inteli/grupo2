ae4a30228b0e88b5ca1753f62c28d31645b2d95c
ItsVasconcellos <fernando.vasconcellos@sou.inteli.edu.br>
2023-03-11 20:44:57 -0300

    Refatorando o código da matriz!

--- Matriz de Eisenhower/script/Node2D.gd
+++ Matriz de Eisenhower/script/Node2D.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-#Variáveis de controle de cada campo da matriz
+#Variáveis de controle de cada quadrante da matriz
+#Imp representa importante e Urg representa urgente, o N serve para representar a negação de um campo
 var impUrg = 0
 var impNurg = 0
 var nImpUrg = 0
@@ -12,76 +13,76 @@ func _ready():
 #Função de controle para o total de horas não passar de 24hrs
 func fazerSomaTotal():
 	return impUrg + impNurg + nImpNurg + nImpUrg
-	
+
+#Função para diminuir uma hora de uma categoria qualquer
+func diminuirValor(categoria,Label,ProgressBar):
+#	Verifica se a categoria não ficará negativa
+	if categoria > 0:
+		#Altera tanto o label quanto a progress bar para a nova quantidade de horas
+		Label.text = "Tempo: " + str(categoria-1) + "h"
+		ProgressBar.value = categoria - 1
+		#Retorna verdadeiro para permitir que seja diminuido 1 da variável global
+		return true
+	#Retorna falso para impedir que seja diminuído 1 da variável global
+	return false
+
+#Função para aumentar uma hora de uma categoria qualquer
+func aumentarValor(categoria,Label,ProgressBar):
+	#Verifica se o aumento não ultrapassará as 24 horas	
+	if(fazerSomaTotal()+1 <= 24):
+		#Altera tanto o label quanto a progress bar para a nova quantidade de horas
+		Label.text = "Tempo: " + str(categoria+1) + "h"
+		ProgressBar.value = categoria + 1
+		#Retorna verdadeiro para permitir que seja aumentado 1 da variável global
+		return true
+	#Retorna falso para impedir que seja aumentado 1 da variável global
+	return false
+
 func _on_BtnImpUrgMenos_pressed():
-	if impUrg != 0:
+	#Chama a função para a verificar se é possível diminuir a variável ImpUrg
+	if diminuirValor(impUrg,$LblImportanteUrgente,$ProgressBarImpUrg):
 		impUrg -= 1
-#		Retângulo q acenderá 
-		$LblImportanteUrgente.text = "Tempo: " + str(impUrg) + "h"
-		$ProgressBarImpUrg.value = impUrg
 
 func _on_BtnImpUrgMais_pressed():
-	impUrg += 1
-	if(fazerSomaTotal() <= 24):
-		#Retângulo pintando
-		$LblImportanteUrgente.text = "Tempo: " + str(impUrg) + "h"
-		$ProgressBarImpUrg.value = impUrg
-	else:
-		impUrg -=1
+	#Chama a função para a verificar se é possível aumentar a variável ImpUrg
+	if aumentarValor(impUrg,$LblImportanteUrgente,$ProgressBarImpUrg):
+		impUrg +=1	
 
 func _on_BtnImpNurgMenos_pressed():
-	if impNurg != 0:
+	#Chama a função para a verificar se é possível diminuir a variável ImpNurg
+	if diminuirValor(impNurg,$LblImportanteNUrgente,$ProgressBarImpNUrg):
 		impNurg -= 1
-#		Retângulo q acenderá 
-		$LblImportanteNUrgente.text = "Tempo: " + str(impNurg) + "h"
-		$ProgressBarImpNUrg.value = impNurg
-
 
 func _on_BtnImpNurgMais_pressed():
-	impNurg += 1
-	if(fazerSomaTotal() <= 24):
-		#Retângulo pintando
-		$LblImportanteNUrgente.text = "Tempo: " + str(impNurg) + "h"
-		$ProgressBarImpNUrg.value = impNurg
-	else:
-		impNurg -=1
+	#Chama a função para a verificar se é possível aumentar a variável ImpNurg
+	if aumentarValor(impNurg,$LblImportanteNUrgente,$ProgressBarImpNUrg):
+		impNurg +=1
 
 func _on_BtnNImpUrgMenos_pressed():
-	if nImpUrg != 0:
+	#Chama a função para a verificar se é possível diminuir a variável NImpUrg
+	if diminuirValor(nImpUrg,$LblNImportanteUrgente,$ProgressBarNImpUrg):
 		nImpUrg -= 1
-#		Retângulo q acenderá 
-		$LblNImportanteUrgente.text = "Tempo: " + str(nImpUrg) + "h"
-		$ProgressBarNImpUrg.value = nImpUrg
-
 
 func _on_BtnNImpUrgMais_pressed():
-	nImpUrg += 1
-	if(fazerSomaTotal() <= 24):
-		#Retângulo pintando
-		$LblNImportanteUrgente.text = "Tempo: " + str(nImpUrg) + "h"
-		$ProgressBarNImpUrg.value = nImpUrg
-	else:
-		nImpUrg -=1
+	#Chama a função para a verificar se é possível aumentar a variável NImpUrg
+	if aumentarValor(nImpUrg,$LblNImportanteUrgente,$ProgressBarNImpUrg):
+		nImpUrg +=1
 		
 func _on_BtnNImpNUrgMenos_pressed():
-	if nImpNurg != 0:
+	#Chama a função para a verificar se é possível diminuir a variável NImpNUrg
+	if diminuirValor(nImpNurg,$LblNImportanteNUrgente,$ProgressBarNImpNUrg):
 		nImpNurg -= 1
-#		Retângulo q acenderá 
-		$LblNImportanteNUrgente.text = "Tempo: " + str(nImpNurg) + "h"
-		$ProgressBarNImpNUrg.value = nImpNurg
-
 
 func _on_BtnNImpNUrgMais_pressed():
-	nImpNurg += 1
-	if(fazerSomaTotal() <= 24):
-		#Retângulo pintando
-		$LblNImportanteNUrgente.text = "Tempo: " + str(nImpNurg) + "h"
-		$ProgressBarNImpNUrg.value = nImpNurg
-	else:
-		nImpNurg -=1
+	#Chama a função para a verificar se é possível aumentar a variável NImpNUrg
+	if aumentarValor(nImpNurg,$LblNImportanteNUrgente,$ProgressBarNImpNUrg):
+		nImpNurg +=1
 
-
+#Envia as variáveis para a análise da matriz, dando um feedback para o usuário sobre a 
+#organização de suas tarefas
 func _on_Button_pressed():
+	
+	#Análise da categoria Importante e Urgente, dando seu feedback dependendo da quantidade de horas	
 	if impUrg > 6:
 		$LblImportanteUrgente.text ="Você tem muitas tarefas urgentes e importantes! Tente rever as suas prioridades e repensar na importância de cada uma."
 	elif impUrg < 4:
@@ -89,6 +90,7 @@ func _on_Button_pressed():
 	else:
 		$LblImportanteUrgente.text = "Você tem um bom equilíbrio de tarefas urgentes e importantes! Continue a priorizar essas tarefas e mantenha o bom trabalho."
 	
+	#Análise da categoria Importante e Não Urgente, dando seu feedback dependendo da quantidade de horas
 	if impNurg > 15:
 		$LblImportanteNUrgente.text = "Você tem muitas tarefas importantes que não são urgentes! Isso dificulta a sua organização, o que pode tornar essas tarefas urgentes no futuro."
 	elif impNurg < 13:
@@ -96,16 +98,16 @@ func _on_Button_pressed():
 	else:
 		$LblImportanteNUrgente.text = "Você tem um bom número de tarefas importantes que não são urgentes! Certifique-se de continuar a trabalhar nessas tarefas e mantenha o bom trabalho."
 	
+	#Análise da categoria Não Importante e Urgente, dando seu feedback dependendo da quantidade de horas
 	if nImpUrg > 5:
 		$LblNImportanteUrgente.text = "Você tem muitas tarefas não importantes que são urgentes! Tente delegar ou eliminar essas tarefas da sua lista para ter um melhor aproveitamento do seu tempo."
 	elif nImpUrg < 3:
 		$LblNImportanteUrgente.text = "Você não tem muitas tarefas não importantes que são urgentes! Certifique-se de priorizar as tarefas que são realmente importantes para você."
 	else:
 		$LblNImportanteUrgente.text = "Você tem um bom número de tarefas não importantes que são urgentes! Continue a se organizar dessa maneira e mantenha o bom trabalho."
-	
+
+	#Análise da categoria Não Importante e Não Urgente, dando seu feedback dependendo da quantidade de horas
 	if nImpNurg > 2:
 		$LblNImportanteNUrgente.text = "Você tem muitas tarefas não importantes não urgentes! Tente rever suas prioridades e reavaliar a importância das suas tarefas."
-	elif nImpNurg < 1:
-		$LblNImportanteNUrgente.text = "Você não tem muitas tarefas não importantes não urgentes! Considere adicionar tarefas que possam melhorar sua vida pessoal ou profissional no futuro."
 	else:
 		$LblNImportanteNUrgente.text = "Você tem um bom número de tarefas não importantes não urgentes! Continue a priorizar as tarefas certas e mantenha o bom trabalho."
