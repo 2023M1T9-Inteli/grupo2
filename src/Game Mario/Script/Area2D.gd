e44965cdf8106c942f8f9d1881fb598b031b5ee9
Edustn <eduardo.santos@sou.inteli.edu.br>
2023-03-12 12:40:12 -0300

    Mudanca nos comentarios e organizacao das pastas

--- src/Game Mario/Script/Area2D.gd
+++
@@ -1,4 +0,0 @@
-extends Area2D
-
-func _on_Area2D_body_entered(body):
-	get.node()
--- src/Game Mario/Script/popUpRelogio.gd
+++ src/Game Mario/Script/popUpRelogio.gd
@@ -4,9 +4,6 @@ extends Popup
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 #Função para ativar o popUp na tela
 func ativaPopUp():
