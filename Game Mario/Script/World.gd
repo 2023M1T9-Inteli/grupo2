11c6c9887f2962193aa1ce88e10cf6c822a844da
ItsVasconcellos <fernando.vasconcellos@sou.inteli.edu.br>
2023-03-11 21:13:36 -0300

    Adicionando comentários ao código do game mario!

--- Game Mario/Script/World.gd
+++ Game Mario/Script/World.gd
@@ -1,5 +1,17 @@
 extends Node2D
+#Variável de controle para saber se o vídeo tutorial deve ser exibido ou não
+var video = true
 
 func _ready():
-	pass
-	
+	#Esconde o tutorial para que o jogador não veja antes de ver o primeiro inimigo
+	$"popUpRelogio".hide()
+
+func _process(_delta):
+	#Verifica se deve mostrar o vídeo
+	if(video):
+		#Verifica a posição do jogador		
+		if($Player.position.x >= 475):
+			#Chama a função do popUp para mostrar o tutorial para o player 
+			$popUpRelogio/Popup.ativaPopUp()
+			#Desativa a possibilidade de mostrar o vídeo novamente, permitindo assim que o usuário feche o vídeo
+			video = false
