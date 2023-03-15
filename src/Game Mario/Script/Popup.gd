654ac235b9245d50a8a9302f846db2ca5a92af26
ItsVasconcellos <fernando.vasconcellos@sou.inteli.edu.br>
2023-03-15 14:56:15 -0300

    Teste!

--- src/Game Mario/Script/Popup.gd
+++ src/Game Mario/Script/Popup.gd
@@ -4,5 +4,5 @@ func _ready():
 	pass 
 
 # ativa o pop up quando o botão de controles é pressionado na tela inicial. Essa função é o que o ativa:
-func ativaPopUp():
+func ativa_pop_up():
 	popup_centered()
