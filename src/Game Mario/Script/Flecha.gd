1309b624d70cabc5db6c35600ebacfe42c04555b
ifferreira <ivan.ferreira@sou.inteli.edu.br>
2023-03-22 10:51:25 -0300

    gantt v1

---
+++ src/Game Mario/Script/Flecha.gd
@@ -0,0 +1,41 @@
+extends RigidBody2D
+
+#variável que auxilia na contagem do tempo até a flecha desaparecer
+var tempo = 0
+
+#variável que auxilia no desaparecimento da flecha assim que ele entra em contato com a personagem inimiga
+var desaparecer_flecha = false
+
+onready var global = get_node("/root/GlobalTeste")
+
+func _ready():
+	$AnimatedSprite.play("default")
+
+
+func _process(delta):
+	gravity_scale = 0
+	#acrese delta na variável tempo a cada frame
+	tempo += delta
+	#quando a variável tempo for maior do que 2, faz desaparecer a flecha
+	if tempo > 0.6:
+		$AnimatedSprite.play("new")
+		if global.direcao_do_jogador == true:
+			linear_velocity = Vector2(0, 0)
+		else:
+			linear_velocity = Vector2(0, 0)
+
+	if tempo > 0.7:
+		queue_free()
+
+
+
+#função chamada assim que a flecha entra em contato com um body
+func _on_Area2D_body_entered(body):
+	#se entra em contato com um body pertencente ao grupo monster, faz o body desaparecer e põe a variável desaparecer_flecha como verdadeira
+	if body.is_in_group("monster"):
+		body.dano()
+		desaparecer_flecha = true
+	
+	#se a variável desaparecer_flecha for verdadeira, a flecha desaparece
+	if desaparecer_flecha:
+		queue_free()
--- src/Game Mario/Script/Mapa_Gantt.gd
+++ src/Game Mario/Script/Mapa_Gantt.gd
@@ -1,10 +1,46 @@
 extends Node2D
 
 onready var fase = get_node("/root/CategoriaGlobal")
+var jogador = preload("res://Branch/Player.tscn").instance()
+var pode_atirar = false
+
+
 
 func _ready():
 	fase.level = "fase 3"
+	jogador.position = Vector2(-1844, 526)
+	add_child(jogador)
+	$KinematicBody2D/AnimatedSprite.play("default")
+
 
 
 func _process(delta):
 	$Player.morte_queda()
+	atirar_flecha()
+
+
+
+func atirar_flecha():
+	#a função só retorna algo se o botão ui_atirar for pressionado e se a variável pode_atirar for verdadeira
+	if Input.is_action_just_pressed("ui_atirar") and pode_atirar:
+
+		#intancia a cena Flecha e joga dentro da variável cena_flecha
+		var cena_flecha = preload("res://Cenas/Flecha.tscn").instance()
+		
+		#se o sprite da personagem estiver voltado para a esquerda, a flecha vai para a esquerda
+		if $Player/Sprite.flip_h == true:
+			cena_flecha.position = Vector2($Player.position.x - 55, $Player.position.y)
+			cena_flecha.linear_velocity = (Vector2(-750, 0))
+		
+		#senão, a flecha vai para a direita
+		else:
+			cena_flecha.position = Vector2($Player.position.x + 55, $Player.position.y)
+			cena_flecha.linear_velocity = (Vector2(750, 0))
+		
+		#adiciona a cena Flecha, que foi instanciada, na cena atual
+		get_tree().current_scene.add_child(cena_flecha)
+		
+
+func _on_Area2D_body_entered(body):
+	pode_atirar = true
+	$KinematicBody2D/AnimatedSprite.play("new")
